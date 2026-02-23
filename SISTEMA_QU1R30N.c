af83751d5328faa157bc5bd303407bbfb0a2c9fb
qu1r30n <qu1r30n@hotmail.com>
2026-02-23 11:46:15 -0600

    ajustes para  ya empesar con el conmutador

--- SISTEMA_QU1R30N.c
+++ SISTEMA_QU1R30N.c
@@ -6,13 +6,62 @@
 
 #include "cabeceras/cabeceras_procesos/00_cabeceras_del_sistema/var_fun_GG.h"
 
-
+const char** G_caracter_separacion = GG_caracter_separacion;
 // Inicialización
-void inicializacion() {
+void inicializacion() 
+{
     crearArchivo(G_archivos[ARCH_INVENTARIO][0], G_archivos[ARCH_INVENTARIO][1]);
     for(int i=0;i<2;i++)
         crearArchivo(G_archivos_registros[i][0], G_archivos_registros[i][1]);
 }
+
+
+void procesar_opcion(char* texto_prueba)
+{
+    int n_opciones = 0;
+    char** opciones = modelo_split(texto_prueba, G_caracter_separacion[0]);
+
+    if (strcmp(opciones[0], "op_tienda"))
+    {
+        char** sub_opcion = modelo_split(texto_prueba, G_caracter_separacion[1]);
+        if (sub_opcion == NULL || sub_opcion[0] == NULL)
+            return;
+
+        if (strcmp(sub_opcion[0], "ventas") == 0) 
+        {
+            venta(sub_opcion[1]);
+        } 
+        else if (strcmp(sub_opcion[0], "compras") == 0) 
+        {
+            compra(sub_opcion[1]);
+        } 
+        else if (strcmp(sub_opcion[0], "agregar_producto") == 0) 
+        {
+            agregarProducto(sub_opcion[1]);
+        }
+        else 
+        {
+            printf("Opción no válida: %s\n", sub_opcion[0]);
+        }
+    }
+    else if (strcmp(opciones[0], "procesos_generales"))
+    {
+        // entrada salidad de dinero
+        //SOLO CON EL DINERO - NO CON PRODUCTOS NI CON SERVICIOS SOLO CON EL DINERO 
+        //pero talves impuestos y todo lo que un administrador y contador utilizaria lo mas general en realidad no se si esto iria aqui o en otro dedicado a eso
+        
+    }
+    else if (strcmp(opciones[0], "procesos_sistema"))
+    {
+        //aqui se habla directamente con el sistema solo el programador
+    }
+    
+
+    free_split(opciones, n_opciones);
+
+}
+
+
 
 
 int main() 
@@ -20,41 +69,17 @@ int main()
     //inicializacion();
 
     char* texto_prueba = "compras|2|3|4|5|6";
-// Opción como string con varias acciones separadas por '|'
-    char* opcion;
 
-    while(1) 
-	{ 
-        // while true para usar en un futuro
-        // Hacemos split de la cadena de opciones
-        int n_opciones = 0;
-        char** opciones = modelo_split(texto_prueba, "|");
-        
-        
-            if(strcmp(opciones[0], "ventas") == 0) {
-                
-                venta(opciones[1]);
-                
-            } 
-            else if(strcmp(opciones[0], "compras") == 0) {
-                
-                compra(opciones[1]);
-                
-            } 
-            else if(strcmp(opciones[0], "agregar_producto") == 0) 
-			{
-                
-                agregarProducto(opciones[1]);
-                
-            }
-            // Puedes agregar más opciones si quieres
-        
+    
+    
 
-        free_split(opciones, n_opciones);
-        
-        modelo_delay_ms("1000");
-        
-	}
+    // Llamamos al conmutador
+    procesar_opcion(texto_prueba);
+
+    
+    modelo_delay_ms("1000");       
+	
+
 
     return 0;
 }
--- procesos/00_procesos_del_sistema/var_fun_GG.c
+++ procesos/00_procesos_del_sistema/var_fun_GG.c
@@ -1,13 +1,14 @@
 #include "../../cabeceras/cabeceras_procesos/00_cabeceras_del_sistema/var_fun_GG.h"
 
-const char* G_caracter_separacion[5] = { "|", "°", "¬", "╦", "╔" };
+const char* GG_caracter_separacion[] = { "|", "°", "¬", "╦", "╔" };
 
-const char* G_archivos[1][2] = {
+const char* GG_archivos[][2] = {
     { "inventario.txt",
       "ID|Producto|Contenido|TipoMedida|PrecioVenta|CodBarras|Cantidad|CostoCompra|Proveedor|Col10|Col11|Col12|Col13|Col14|Col15|Col16|Col17|Fecha" }
 };
 
-const char* G_archivos_registros[2][2] = {
+
+const char* GG_archivos_registros[][2] = {
     { "registros_ventas.txt", "" },
     { "registros_compras.txt", "" }
 };
